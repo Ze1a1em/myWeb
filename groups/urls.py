1da5a05c059affe324987f5256591822c0d24167
ZS <zelalemsmith@Zelalems-MacBook-Air.local>
2022-04-15 19:49:17 -0500

    Fixed import erros.

--- groups/urls.py
+++ groups/urls.py
@@ -6,7 +6,7 @@ app_name = 'groups'
 
 urlpatterns = [
     path("", views.ListGroups.as_view(), name="all"),
-    path("new/$", views.CreateGroup.as_view(), name="create"),
+    path("new/", views.CreateGroup.as_view(), name="create"),
     re_path("posts/in/(?P<pk>[-\w]+)/$",views.SingleGroup.as_view(),name="single"),
     re_path(r"join/(?P<pk>[-\w]+)/$",views.JoinGroup.as_view(),name="join"),
     re_path(r"leave/(?P<pk>[-\w]+)/$",views.LeaveGroup.as_view(),name="leave"),
