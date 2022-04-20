2675fc9b872c796550f7b348f35230e81ec55a60
ZS <zelalemsmith@Zelalems-MacBook-Air.local>
2022-04-20 16:08:03 -0500

    contact, about me and works added.

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePage.as_view(), name="home"),
-    path("test/", views.TestPage.as_view(), name="test"),
+    path("contact/", views.TestPage.as_view(), name="contact"),
     path("thanks/", views.ThanksPage.as_view(), name="thanks"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
